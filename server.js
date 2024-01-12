877ba2e07b063fbd72cfc54d9555279297422618
Unnati Gupta <unnatig1920@gmail.com>
2024-01-12 18:23:47 +0530

    task: Task 2: MongoDB connectivity

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
-// TASK 1: Setting up server
+// Task 2 : Connecting with mongo db data base 
+const http = require("http");
+const { MongoClient } = require("mongodb");
 
-
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-  res.write('Hello World')
-  res.end()}
+const PORT = 8000;
+const uri =
+  "mongodb+srv://unnatig1920:unnatigupta@cluster0.uokvn7o.mongodb.net/";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("Connected to Mongo Database");
 
+    const server = http.createServer((req, res) => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Hello World\n");
+    });
 
-server.listen(80)
-console.log("Server listening on port 80")
+    server.listen(PORT, () => {
+      console.log("Server running");
+    });
+  } catch (err) {
+    console.error("Could not connect to MongoDB:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
